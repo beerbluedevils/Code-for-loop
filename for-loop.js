970e57378c086378528b3a856b727a59a097c995
beerbluedevils <beerbluedevils@gmail.com>
2020-11-29 21:14:52 +0700

    create js file

---
+++ for-loop.js
@@ -0,0 +1,8 @@
+//Learning for loop
+const num1 = [ 123, 462, 23, 517, 664 ];
+const num2 = [ 742, 982, 244, 520, 112 ];
+
+for (let i = 0; i < num1.length; i++){
+	let sum = num1[i] + num2[i];
+	console.log(sum);
+}
